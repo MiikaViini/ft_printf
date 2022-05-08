0b09a1e744fce58b7dcde36d4e50f37a3a763c90
mviinika <mviinika@hive.student.fi>
2022-05-08 22:18:41 +0300

    null treatment b

--- libft/ft_dectobin.c
+++ libft/ft_dectobin.c
@@ -6,37 +6,43 @@
 /*   By: mviinika <mviinika@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 13:48:10 by mviinika          #+#    #+#             */
-/*   Updated: 2022/05/08 21:56:40 by mviinika         ###   ########.fr       */
+/*   Updated: 2022/05/08 22:18:19 by mviinika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static long long	treat_minus(long long num, int *sign)
+{
+	if (num == -32768)
+		num++;
+	else if (num == -2147483648)
+		num++;
+	else if (num == -9223372036854775807 - 1)
+		num++;
+	num = num * -1;
+	*sign = 1;
+	return (num);
+}
+
 char	*ft_dectobin(long long num)
 {
 	char				*bin;
 	size_t				i;
-	unsigned long long	min;
+	int					sign;
 
 	i = 0;
 	bin = ft_strnew(64);
-	min = num;
+	sign = 0;
+	bin[0] = '0';
 	if (num < 0)
+		num = treat_minus(num, &sign);
+	while (num)
 	{
-		if (num == -32768)
-			num++;
-		else if (num == -2147483648)
-			num++;
-		else if (num == -9223372036854775807 - 1)
-			num++;
-		min = num * -1;
+		bin[i++] = num % 2 + '0';
+		num /= 2;
 	}
-	while (min > 0)
-	{
-		bin[i++] = min % 2 + '0';
-		min /= 2;
-	}
-	if (num < 0)
+	if (sign)
 		bin[i++] = '-';
 	return (ft_strrev(bin));
 }
